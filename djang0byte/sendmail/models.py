78d01489275e6bb030a5e72ee61fbc5eecae9af8
Vladimir Yakovlev <nvbn.rm@gmail.com>
2011-05-03 21:15:03 +0400

    fix mails format

--- djang0byte/sendmail/models.py
+++ djang0byte/sendmail/models.py
@@ -1,22 +1,27 @@
 from django.conf import settings
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
 from django.db import models
 
 
 class Mails(models.Model):
+    """Mail class"""
     subject = models.CharField(max_length=1000)
     message = models.TextField()
     recipient = models.EmailField()
 
     def send(self):
+        """Send message"""
         try:
-            send_mail(self.subject, self.message, settings.DEFAULT_FROM_EMAIL, [self.recipient,])
+            #send_mail(self.subject, self.message, settings.DEFAULT_FROM_EMAIL, [self.recipient,])
+            msg = EmailMessage(self.subject, self.message, settings.DEFAULT_FROM_EMAIL, [self.recipient,])
+            msg.content_subtype = "html"  # Main content is now text/html
+            msg.send()
         except:
             pass
 
     @staticmethod
     def send_all():
+        """Send all messages and delete sended"""
         for mail in Mails.objects.all():
-            print mail.subject
             mail.send()
             mail.delete()
