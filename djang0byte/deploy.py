12ece1aa27e1c7d0132a775c9373ae231021e752
Divius <divius.inside@gmail.com>
2011-09-11 18:53:58 +0400

    Fix deploy.py on clean systems
    The following issues are fixed:
    1. Do not import anything involving Django before dependencies are installed
    2. Always check result of os.system as it doesn't raise exceptions on failure
    3. Do not mention django-simplepagination twice in deps
    4. Also depend on pytz, BeautifulSoup and south
    5. Add trailing newline

--- djang0byte/deploy.py
+++ djang0byte/deploy.py
@@ -3,16 +3,24 @@
 import os
 import sys
 sys.path.append(os.getcwd())
-import settings
-from main.models import Profile
-from django.contrib.auth.models import User
-from treemenus.models import Menu
 
 if __name__ == '__main__':
     deps = open('deps', 'r')
     for dep in deps.readlines():
-        os.system('pip install %s' % (dep,))
-    os.system('python manage.py syncdb')
+        if os.system('pip install %s' % (dep,)) != 0:
+            sys.exit("Unable to install dependency: %s "
+                "consider logging in as a superuser." % dep)
+
+    os.environ["DJANGO_SETTINGS_MODULE"] = "settings"
+
+    import settings
+    from main.models import Profile
+    from django.contrib.auth.models import User
+    from treemenus.models import Menu
+
+    if os.system('python manage.py syncdb') != 0:
+        sys.exit("Unable to execute syncdb")
+
     Profile.objects.create(
         user=User.objects.get()
     )
