b3f4250761723e44832473c7029ddd880668ca1a
Vladimir Yakovlev <nvbn.rm@gmai.com>
2011-11-29 03:56:15 +0400

    add accounts merging

--- djang0byte/register/models.py
+++ djang0byte/register/models.py
@@ -1,3 +1,53 @@
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.utils.translation import ugettext as _
 from django.db import models
+from datetime import datetime
+import hashlib
+from main.models import Profile
 
-# Create your models here.
+
+class MergeKeyManager(models.Manager):
+    def create(self, **kwargs):
+        if not 'key' in kwargs and 'user' in kwargs:
+            kwargs['key'] = MergeKey.generate_key(kwargs['user'])
+        super(MergeKeyManager, self).create(**kwargs)
+
+
+class MergeKey(models.Model):
+    """Users merge key"""
+    user = models.ForeignKey(User, verbose_name=_('user'))
+    key = models.CharField(
+        max_length=32, editable=False, unique=True, verbose_name=_('key')
+    )
+    objects = MergeKeyManager
+
+    @staticmethod
+    def generate_key(user):
+        return hashlib.md5(
+            "%d_%s_%s" % (user.id, user.username, str(datetime.now()))
+        ).hexdigest()
+
+    def merge(self, new_user):
+        for ct in ContentType.objects.all():
+            model = ct.model_class()
+            for field, _model in model._meta.get_fields_with_model().items():
+                if _model == User:
+                    model.objects.filter(**{
+                        field.name: self.user
+                    }).update(**{
+                        field.name: new_user
+                    })
+                elif _model == Profile:
+                    model.objects.filter(**{
+                        field.name: self.user.get_profile()
+                    }).update(**{
+                        field.name: new_user.get_profile()
+                    })
+        self.delete()
+        self.user.delete()
+
+    def save(self, *args, **kwargs):
+        if not self.id and not self.key:
+            self.key = MergeKey.generate_key(self.user)
+        super(MergeKey, self).save(*args, **kwargs)
--- djang0byte/register/views.py
+++ djang0byte/register/views.py
@@ -1,16 +1,17 @@
+from papyon.profile import Profile
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from annoying.decorators import ajax_request
+from annoying.decorators import ajax_request, render_to
 from django.contrib.auth.views import logout_then_login
-from django import http
-from django.contrib import messages, auth
-from django.shortcuts import redirect, render_to_response
-from django.template.context import RequestContext
+from django.contrib import auth
+from django.shortcuts import redirect, get_object_or_404
 from loginza import signals, models
 from loginza.models import UserMap
-from loginza.templatetags.loginza_widget import _return_path
-from main.models import Profile
 from django.contrib.auth.views import login as original_login
+from register.models import MergeKey
+from sendmail.models import Mails
+from django.utils.translation import ugettext as _
 
 @ajax_request
 def check(request, type, value, value_2 = None):
@@ -57,6 +58,46 @@ def check(request, type, value, value_2 = None):
 @login_required
 def logout(request):
     return logout_then_login(request,'/')
+
+@login_required
+@ajax_request
+def generate_merge_key(request):
+    key = MergeKey.objects.create(
+        user=request.user,
+    )
+    Mails.objects.create(
+        subject=_('You try to merge accounts'),
+        message=_('For merging account %(username)s with another go to this %(url)s,' \
+                  ' for merging you need to login to main user' % {
+            'username': request.user.username,
+            'url': getattr(settings, 'SITE_URL') + '/merge_accounts/?key=' + key.key,
+        }),
+        recipient=request.user,
+    )
+    return {
+        'status': True,
+    }
+
+@login_required
+@render_to('registration/merge.html')
+def merge(request):
+    key = get_object_or_404(
+        MergeKey,
+        key=request.GET.get('key', None),
+    )
+    if key.user == request.user:
+        return {
+            'relogin': True
+        }
+    try:
+        key.merge(request.user)
+        return {
+            'succes': True
+        }
+    except (User.DoesNotExist, Profile.DoesNotExist):
+        return {
+            'error': True
+        }
 
 def loginza_auth_handler(sender, user, identity, **kwargs):
     try:
